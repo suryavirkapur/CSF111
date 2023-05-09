c626148023eb3666623d138e84c38ed4da4cc354
Suryavir Kapur <52126281+suryavirkapur@users.noreply.github.com>
2023-05-09 09:33:08 +0400

    Update 1.c

--- Week10/1.c
+++ Week10/1.c
@@ -27,29 +27,3 @@ void change(int n)
   printf("\n After adding value inside function = %d", n);
 }
 
-// Program 1(b)
-#include <stdio.h>
-
-void change(int *num);
-int main()
-{
-  int x;
-
-  printf("\n Enter a number: ");
-  scanf("%d", &x);
-
-  printf("\n Before function call 'x' = %d", x);
-
-  change(&x);
-
-  printf("\n After function call 'x' = %d", x);
-
-  return 0;
-}
-
-void change(int *num)
-{
-  printf("\n Before adding value inside function 'x' = %d", *num);
-  (*num) += 100;
-  printf("\n After adding value inside function 'x' = %d", *num);
-}
